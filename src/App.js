397e1bfb9fd3af00183d68c5ce0cceb861a30897
bmjoy85705 <u1702042@student.cuet.ac.bd>
2021-07-21 23:26:57 +0600

    core concep reboosted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { useState, useEffect } from 'react';
+
+const products = [{name:"photoshop",price:'$40'},
+{name:'Figma', price:"$20"},
+{name:'illustrator',price:'$30'},
+{name:'adobe premiere pr0',price:"$20"}];
+
+
+
+function Paragraph(props){
+  const producStyle = {
+    border:'1px solid red',
+    backgroundColor:'lightsalmon',
+    width:400,
+    height:300,
+    float: 'left',
+    margin:'2px',
+    padding:'5px'
+
+
+  }
+  const[count,setCount] = useState(0);
+  
+let countValue = () =>{
+  let value = count+1 ;
+  setCount(value);
+}
+  return(
+  
+   <div style={producStyle}>
+     
+     <h1>{props.product.name}</h1>
+    <h3>{props.product.email}</h3>
+  <h6>{props.product.phone}</h6>
+  <h4>{count}</h4>
+    <button onClick={()=>{countValue()}}>Buy now</button>
+   </div>
+  )
+};
+function Users (){
+  const [users ,setUsers] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=> setUsers(data))
+  },[])
+  return(
+    <div>
+     {
+       users.map(user=> <Paragraph product={user}></Paragraph>)
+     }
+    </div>
+  )
+}
+
+
+function App(data) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Users></Users>
+      {
+       products.map(product =><Paragraph product={product}></Paragraph>)
+      }
+      
+       
+      
     </div>
   );
 }
